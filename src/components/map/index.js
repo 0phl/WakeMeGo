75b761c146b55647387a82f829b5e97232cd1fa9
Ronan <ronan.delacruz@sdca.edu.ph>
2025-07-23 23:57:15 +0800

    export all map components

---
+++ src/components/map/index.js
@@ -0,0 +1,7 @@
+// Export all map components
+export { default as InteractiveMap } from './InteractiveMap';
+export { default as DraggablePin } from './DraggablePin';
+export { default as GeofenceCircle } from './GeofenceCircle';
+export { default as LocationMarker } from './LocationMarker';
+export { default as PathTrail } from './PathTrail';
+export { default as DistanceIndicator } from './DistanceIndicator';
