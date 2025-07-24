d10f1ca9715aab849b98f0b7573608e8044012fc
Ronan <ronan.delacruz@sdca.edu.ph>
2025-07-24 12:56:28 +0800

    export all screen

---
+++ src/screens/onboarding/index.js
@@ -0,0 +1,4 @@
+// Export all onboarding screens
+export { default as SplashScreen } from './SplashScreen';
+export { default as WelcomeScreen } from './WelcomeScreen';
+export { default as TutorialScreen } from './TutorialScreen';
